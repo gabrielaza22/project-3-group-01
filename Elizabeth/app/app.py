a546f930fdab628fbd65bb9352f3a33cc33eb64f
Elizabeth Viramontes <162919015+ElizabethVira@users.noreply.github.com>
2024-08-08 19:20:37 -0500

    update

--- Elizabeth/app/app.py
+++ Elizabeth/app/app.py
@@ -16,6 +16,19 @@ sql = SQLHelper()
 def index():
     return render_template("index.html")
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+@app.route("/about_us")
+def about_us():
+    return render_template("about_us.html")
+
+
 @app.route('/api/v1.0/get_sunburst_data')
 def get_sunburst_data():
     # Obtain the data from get_sunburst from SQLHelper
@@ -38,16 +51,32 @@ if __name__ == '__main__':
 
 
 
+
+
+# @app.route('/api/v1.0/get_sunburst_data')
+# def get_sunburst_data():
+
+#     sunburst_data = sql.get_sunburst()
+    
+#     data = {
+#         "ids": [item['id'] for item in sunburst_data],
+#         "labels": [item['label'] for item in sunburst_data],
+#         "parents": [item['parent'] for item in sunburst_data]
+#     }
+    
+#     return jsonify(data)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
 # http://localhost:5000/api/v1.0/get_sunburst_data //In case need to see if is reading the data
 
-# @app.route("/dashboard")
-# def dashboard():
-#     return render_template("dashboard.html")
-
-# @app.route("/map")
-# def map():
-#     return render_template("map.html")
-
-# @app.route("/about_us")
-# def about_us():
-#     return render_template("about_us.html")
